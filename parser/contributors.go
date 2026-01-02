e5d71456fb2409b03181578018944a7ad37166e2
nikita zigman <zigman.nikita@gmail.com>
2026-01-02 21:29:28 +0000

    remove old parser

--- parser/contributors.go
+++
@@ -1,7 +0,0 @@
-package parser
-
-type Contributor struct {
-	Name     Name     `yaml:"name" validate:"name"`
-	LastName LastName `yaml:"last_name" validate:"last_name"`
-	Email    Email    `yaml:"email" validate:"email_addr"`
-}
--- parser/defaults.go
+++
@@ -1,8 +0,0 @@
-package parser
-
-type Default struct {
-	Tags         []Tag         `yaml:"tags" validate:"dive,tag"`
-	Link         *Link         `yaml:"link" validate:"omitempty,link"`
-	Language     *Language     `yaml:"language" validate:"omitempty,language"`
-	Contributors []Contributor `yaml:"contributors" validate:"dive"`
-}
